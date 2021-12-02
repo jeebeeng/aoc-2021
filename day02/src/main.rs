11d3268304409be6a1bd8f5a21ffeb1b8dc991ac
John Bang <jeebeeng@gmail.com>
2021-12-02 14:01:55 -0500

    Complete part 2 of Day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -5,6 +5,7 @@ use std::str;
 fn main() -> Result<(), Box<dyn Error>> {
     let data = fs::read_to_string("../input.txt")?;
     println!("Part one solution: {}", part_one(&data));
+    println!("Part two solution: {}", part_two(&data));
     Ok(())
 }
 
@@ -21,6 +22,26 @@ fn part_one(lines: &str) -> i32 {
         } else if line.starts_with("up") {
             let value: i32 = line[line.len()-1..].parse().unwrap();
             depth -= value;
+        }
+    }
+    hori * depth
+}
+
+fn part_two(lines: &str) -> i32 {
+    let mut hori = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for line in lines.lines() {
+        if line.starts_with("forward") {
+            let value: i32 = line[line.len()-1..].parse().unwrap();
+            hori += value;
+            depth += aim * value;
+        } else if line.starts_with("down") {
+            let value: i32 = line[line.len()-1..].parse().unwrap();
+            aim += value;
+        } else if line.starts_with("up") {
+            let value: i32 = line[line.len()-1..].parse().unwrap();
+            aim -= value;
         }
     }
     hori * depth
