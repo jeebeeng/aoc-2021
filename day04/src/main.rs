18d9d818ec1c9b38c9bd52066661e7e551e0a291
John Bang <jeebeeng@gmail.com>
2021-12-08 15:56:34 -0500

    Complete part 2 of Day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -5,16 +5,19 @@ use std::str;
 const SIZE: i8 = 5;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let data = fs::read_to_string("../input.txt")?;
+    let data = fs::read_to_string("/home/bang/projects/aoc-2021/day04/input.txt")?;
     println!("Part one solution: {}", part_one(&data));
-    println!("Part two solution: {}", part_two(&data));
+    let result = part_two(&data);
+    println!("Part two solution: {}", result);
     Ok(())
 }
 
 struct Bingo {
     pub drawn: Vec<u8>,
     pub boards: Vec<Vec<Vec<u8>>>,
+    pub won_boards: Vec<Vec<Vec<u8>>>,
     pub curr_drawn: u8,
+    pub last_drawn: u8,
 }
 
 impl Bingo {
@@ -52,7 +55,7 @@ impl Bingo {
         }
         boards.push(board);
 
-        Bingo { drawn, boards, curr_drawn: 0 }
+        Bingo { drawn, boards, won_boards: Vec::new(), curr_drawn: 0, last_drawn: 0, }
     }
 
     pub fn check_win(board: &Vec<Vec<u8>>, drawn: &[u8]) -> bool {
@@ -79,20 +82,29 @@ impl Bingo {
         false
     }
 
-    fn check_boards(&self) -> Vec<Vec<u8>> {
-        let winner: Vec<Vec<u8>> = Vec::new();
+    fn check_boards(&mut self) -> Vec<Vec<Vec<u8>>> {
+        let mut winners: Vec<Vec<Vec<u8>>> = Vec::new();
 
-        for board in &self.boards {
-            if Bingo::check_win(&board, &self.drawn[0..self.curr_drawn as usize]) {
-                return board.clone();
+        self.boards.retain(|board| {
+            let mut keep = true;
+            if Bingo::check_win(board, &self.drawn[0..self.curr_drawn as usize]) {
+                self.won_boards.push(board.clone());
+                winners.push(board.clone());
+                self.last_drawn = self.curr_drawn;
+                keep = false;
             }
-        }
-        winner
+            keep
+        });
+        winners
     }
 
-    pub fn draw(&mut self) -> Vec<Vec<u8>> {
-        self.curr_drawn += 1;
-        self.check_boards()
+    pub fn draw(&mut self) -> Vec<Vec<Vec<u8>>> {
+        if self.curr_drawn as usize != self.drawn.len() {
+            self.curr_drawn += 1;
+            return self.check_boards();
+        } 
+
+        Vec::new()
     }
 }
 
@@ -114,15 +126,21 @@ fn score(board: &Vec<Vec<u8>>, drawn: &[u8]) -> u32 {
 
 fn part_one(lines: &str) -> u32 {
     let mut bingo = Bingo::new(lines);
-    let mut winner: Vec<Vec<u8>> = Vec::new();
+    let mut winners: Vec<Vec<Vec<u8>>> = Vec::new();
 
-    while winner.is_empty() {
-        winner = bingo.draw();
+    while winners.is_empty() {
+        winners = bingo.draw();
     }
-
-    score(&winner, &bingo.drawn[0..bingo.curr_drawn as usize])
+    let winners = winners.get(0).unwrap();
+    score(&winners, &bingo.drawn[0..bingo.curr_drawn as usize])
 }
 
-fn part_two(lines: &str) -> i32 {
-    1
+fn part_two(lines: &str) -> u32 {
+    let mut bingo = Bingo::new(lines);
+
+    while !bingo.boards.is_empty() {
+        bingo.draw();
+    }
+    let winner: &Vec<Vec<u8>> = bingo.won_boards.get(bingo.won_boards.len() - 1).unwrap();
+    score(&winner, &bingo.drawn[0..bingo.last_drawn as usize])
 }
