3dbbd8e73f80d55d4a33f20a25f8a383d850c280
John Bang <jeebeeng@gmail.com>
2021-12-10 17:40:12 -0500

    Refactor part one code

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,8 +1,6 @@
 use std::error::Error;
 use std::fs;
 
-const DEFAULT_TIMER: i32 = 6;
-
 fn main() -> Result<(), Box<dyn Error>> {
     let data = fs::read_to_string("../input.txt")?;
     println!("Part one solution: {}", part_one(&data));
@@ -10,56 +8,44 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-#[derive(Clone, Copy, Debug)]
-struct Fish {
-    pub d_timer: i32,
-    pub i_timer: i32,
-}
-
-struct FishList {
-    pub list: Vec<Fish>,
-}
-
-impl FishList {
-    pub fn new(lines: &str) -> FishList {
-        let mut list: Vec<Fish> = Vec::new();
-        for line in lines.lines() {
-            let mut int_lines: Vec<Fish> = line
-                .split(",")
-                .map(|item| Fish { d_timer: DEFAULT_TIMER, i_timer: item.parse().unwrap() })
-                .collect();
-            list.append(&mut int_lines);
-        }
-
-        FishList { list }
-    }
-
-    fn pass_day(&mut self) {
-        for i in 0..self.list.len() {
-            if self.list[i].i_timer == 0 {
-                self.list.push(Fish { d_timer: DEFAULT_TIMER, i_timer: self.list[i].d_timer + 2 });
-                self.list[i].i_timer = self.list[i].d_timer;
-            } else {
-                self.list[i].i_timer -= 1;
-            }
-        }
-    }
-
-    pub fn pass_days(&mut self, days: i32) {
-        if days >= 0 {
-            for i in 0..days {
-                self.pass_day();
-            }
-        }
-    }
-}
-
 fn part_one(lines: &str) -> i32 {
-    let mut fish_list = FishList::new(lines);
-    fish_list.pass_days(80);
-    fish_list.list.len().try_into().unwrap()
+    let mut list: Vec<i32> = lines
+        .replace("\n", "")
+        .split(",")
+        .map(|item| item.parse().unwrap())
+        .collect();
+
+    for _ in 0..80 {
+        for j in 0..list.len() {
+            if list[j] == 0 {
+                list[j] = 6;
+                list.push(8);
+            } else {
+                list[j] -= 1;
+            }
+        }
+    }
+
+    list.len().try_into().unwrap()
 }
 
 fn part_two(lines: &str) -> i32 {
-    0
+    let mut list: Vec<i32> = lines
+        .replace("\n", "")
+        .split(",")
+        .map(|item| item.parse().unwrap())
+        .collect();
+
+    for _ in 0..256 {
+        for j in 0..list.len() {
+            if list[j] == 0 {
+                list[j] = 6;
+                list.push(8);
+            } else {
+                list[j] -= 1;
+            }
+        }
+    }
+
+    list.iter().sum()
 }
